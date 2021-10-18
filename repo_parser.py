85cc14a83b5702e303b628e8511ab9100826fb68
Gabriel Thompson <43486098+Gabriel786432@users.noreply.github.com>
2021-10-18 15:54:05 -0400

    added stuff

---
+++ repo_parser.py
@@ -0,0 +1,33 @@
+def find_all(string, substr):
+    i = 0
+    sublen = len(substr)
+    indexes = []
+
+    while i < len(string) - sublen:
+        if string[i:i+sublen] == substr:
+            indexes.append(i)
+        i += 1
+
+    return indexes
+
+def first_after(str, start, search):
+    searchlen = len(search)
+
+    while start < len(str):
+        if str[start:start+searchlen] == search:
+            return start
+        start += 1
+
+repo_links = {}
+
+# Paste GitHub HTML into repos.html
+for period in [6, 7, 8]:
+    repo_links[period] = []
+    f = open(f'html/pd{period}.html', 'r').read()
+    all_instances = find_all(f, "a data-skip-pjax")
+
+    for index in all_instances:
+        end = first_after(f, index, "tree")
+        repo_links[period].append(f'https://www.github.com{f[index+30:end]}')
+
+print(repo_links)
