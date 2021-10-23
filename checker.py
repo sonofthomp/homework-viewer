afcf069e32aa43916b1b7fd2e308909263bbdb14
Gabriel Thompson <gthompson30@stuy.edu>
2021-10-23 14:13:06 -0400

    updated checker

--- checker.py
+++ checker.py
@@ -21,13 +21,15 @@ for period in repos:
             found = False
             i = 0
             while (not found) and (i < len(check)):
-                url = repo + 'blob/main/' + check[i]
+                url = repo + check[i]
                 r = requests.get(url)
+                name = r.url.split('/')[3]
 
                 if 200 <= r.status_code < 300:
-                    print(f'{chr(9989)} {check[i]} for {url.split("/")[3]}')
+                    print(f'{chr(9989)} {check[i]} for {name}')
                     found = True
                 else:
+                    print(f'{chr(8230)} {check[i]} not found for {name}')
                     i += 1
             if not found:
-                print(f'{chr(10060)} could not find {check[0]} for {url.split("/")[3]}')
+                print(f'{chr(10060)} could not find {check[0]} for {name}')
