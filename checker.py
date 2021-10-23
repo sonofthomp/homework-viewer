293a88ba333721db2983785ca2589687c82a8815
Gabriel Thompson <gthompson30@stuy.edu>
2021-10-23 18:07:41 -0400

    did big stuff

--- checker.py
+++ checker.py
@@ -12,7 +12,11 @@ with open('json/checks.json', 'r') as f:
     data = f.read()
 
 checks = json.loads(data)
-completed = {}
+
+with open('json/completed.json', 'r') as f:
+    data = f.read()
+
+completed = json.loads(data)
 
 for check in checks:
     completed[check[0]] = {}
@@ -32,6 +36,7 @@ for period in repos:
                 name = r.url.split('/')[3]
 
                 if 200 <= r.status_code < 300:
+                    completed[check[0]][name] = {'link': url, 'pd': period}
                     if visual:
                         print(f'{chr(9989)} {check[i]} for {name}')
                     found = True
@@ -40,5 +45,9 @@ for period in repos:
                         print(f'{chr(8230)} {check[i]} not found for {name}')
                     i += 1
             if not found:
+                completed[check[0]][name] = {'link': 'n/a', 'pd': period}
                 if visual:
                     print(f'{chr(10060)} could not find {check[0]} for {name}')
+
+with open('json/completed.json', 'w') as f:
+    json.dump(completed, f, indent=4)
