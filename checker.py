662b92d9f109e1e9d6f0f3620441e50e2a1d2ebf
Gabriel Thompson <gthompson30@stuy.edu>
2021-10-23 17:01:49 -0400

    added stuff

--- checker.py
+++ checker.py
@@ -2,6 +2,7 @@ import json
 import requests
 from repo_updater import first_after
 
+visual = True
 with open('json/repos.json', 'r') as f:
     data = f.read()
 
@@ -11,11 +12,16 @@ with open('json/checks.json', 'r') as f:
     data = f.read()
 
 checks = json.loads(data)
+completed = {}
+
+for check in checks:
+    completed[check[0]] = {}
 
 for period in repos:
     for repo in repos[period]:
         name = repo.split('/')[3]
-        print(f"\nChecking {name}'s repo, {repo}")
+        if visual:
+            print(f"\nChecking {name}'s repo, {repo}")
 
         for check in checks:
             found = False
@@ -26,10 +32,13 @@ for period in repos:
                 name = r.url.split('/')[3]
 
                 if 200 <= r.status_code < 300:
-                    print(f'{chr(9989)} {check[i]} for {name}')
+                    if visual:
+                        print(f'{chr(9989)} {check[i]} for {name}')
                     found = True
                 else:
-                    print(f'{chr(8230)} {check[i]} not found for {name}')
+                    if visual:
+                        print(f'{chr(8230)} {check[i]} not found for {name}')
                     i += 1
             if not found:
-                print(f'{chr(10060)} could not find {check[0]} for {name}')
+                if visual:
+                    print(f'{chr(10060)} could not find {check[0]} for {name}')
